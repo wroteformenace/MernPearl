c0a5116347dd46f7315b6899851af79f71722374
wroteformenace <richardfineman083@gmail.com>
2025-08-18 00:26:10 +0530

    techHelix demo

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import HeroSection from "./components/HeroSection";
 import CompassDock from "./components/Compass";
 import Navbar from "./components/Navbar";
 import UniverseCards from "./components/UniverseCards";
+import TechCubesSection from "./components/TechCubesSection"
 
 export default function App(){
   return (
@@ -12,6 +13,7 @@ export default function App(){
       </div>
       <HeroSection />
       <UniverseCards />
+      <TechCubesSection />
       <CompassDock />
       {/* rest of your sections… */}
     </div>
